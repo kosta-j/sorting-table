7927da86ebd48d812f14d5a5063716a4d110c52d
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-11-14 10:46:14 +0200

    test filling table from json file

--- dist/bundle.js
+++ dist/bundle.js
@@ -62,27 +62,27 @@
     var range = 2;
     function generateCodeFrame(source, start2 = 0, end = source.length) {
       const lines = source.split(/\r?\n/);
-      let count = 0;
+      let count2 = 0;
       const res = [];
       for (let i = 0; i < lines.length; i++) {
-        count += lines[i].length + 1;
-        if (count >= start2) {
-          for (let j = i - range; j <= i + range || end > count; j++) {
+        count2 += lines[i].length + 1;
+        if (count2 >= start2) {
+          for (let j = i - range; j <= i + range || end > count2; j++) {
             if (j < 0 || j >= lines.length)
               continue;
             const line = j + 1;
             res.push(`${line}${" ".repeat(Math.max(3 - String(line).length, 0))}|  ${lines[j]}`);
             const lineLength = lines[j].length;
             if (j === i) {
-              const pad = start2 - (count - lineLength) + 1;
-              const length = Math.max(1, end > count ? lineLength - pad : end - start2);
+              const pad = start2 - (count2 - lineLength) + 1;
+              const length = Math.max(1, end > count2 ? lineLength - pad : end - start2);
               res.push(`   |  ` + " ".repeat(pad) + "^".repeat(length));
             } else if (j > i) {
-              if (end > count) {
-                const length = Math.max(Math.min(end - count, lineLength), 1);
+              if (end > count2) {
+                const length = Math.max(Math.min(end - count2, lineLength), 1);
                 res.push(`   |  ` + "^".repeat(length));
               }
-              count += lineLength + 1;
+              count2 += lineLength + 1;
             }
           }
           break;
@@ -2620,38 +2620,38 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
     if (modifiers.includes("document"))
       listenerTarget = document;
     if (modifiers.includes("prevent"))
-      handler3 = wrapHandler(handler3, (next, e) => {
+      handler3 = wrapHandler(handler3, (next2, e) => {
         e.preventDefault();
-        next(e);
+        next2(e);
       });
     if (modifiers.includes("stop"))
-      handler3 = wrapHandler(handler3, (next, e) => {
+      handler3 = wrapHandler(handler3, (next2, e) => {
         e.stopPropagation();
-        next(e);
+        next2(e);
       });
     if (modifiers.includes("self"))
-      handler3 = wrapHandler(handler3, (next, e) => {
-        e.target === el && next(e);
+      handler3 = wrapHandler(handler3, (next2, e) => {
+        e.target === el && next2(e);
       });
     if (modifiers.includes("away") || modifiers.includes("outside")) {
       listenerTarget = document;
-      handler3 = wrapHandler(handler3, (next, e) => {
+      handler3 = wrapHandler(handler3, (next2, e) => {
         if (el.contains(e.target))
           return;
         if (el.offsetWidth < 1 && el.offsetHeight < 1)
           return;
         if (el._x_isShown === false)
           return;
-        next(e);
+        next2(e);
       });
     }
-    handler3 = wrapHandler(handler3, (next, e) => {
+    handler3 = wrapHandler(handler3, (next2, e) => {
       if (isKeyEvent(event)) {
         if (isListeningForASpecificKeyThatHasntBeenPressed(e, modifiers)) {
           return;
         }
       }
-      next(e);
+      next2(e);
     });
     if (modifiers.includes("debounce")) {
       let nextModifier = modifiers[modifiers.indexOf("debounce") + 1] || "invalid-wait";
@@ -2664,8 +2664,8 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
       handler3 = throttle(handler3, wait);
     }
     if (modifiers.includes("once")) {
-      handler3 = wrapHandler(handler3, (next, e) => {
-        next(e);
+      handler3 = wrapHandler(handler3, (next2, e) => {
+        next2(e);
         listenerTarget.removeEventListener(event, handler3, options);
       });
     }
@@ -3156,7 +3156,259 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   var src_default = alpine_default;
   var module_default = src_default;
 
+  // heroes.json
+  var count = 82;
+  var next = "https://swapi.dev/api/people/?page=2";
+  var previous = null;
+  var results = [
+    {
+      name: "Luke Skywalker",
+      height: "172",
+      mass: "77",
+      hair_color: "blond",
+      skin_color: "fair",
+      eye_color: "blue",
+      birth_year: "19BBY",
+      gender: "male",
+      homeworld: "https://swapi.dev/api/planets/1/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: [],
+      vehicles: [
+        "https://swapi.dev/api/vehicles/14/",
+        "https://swapi.dev/api/vehicles/30/"
+      ],
+      starships: [
+        "https://swapi.dev/api/starships/12/",
+        "https://swapi.dev/api/starships/22/"
+      ],
+      created: "2014-12-09T13:50:51.644000Z",
+      edited: "2014-12-20T21:17:56.891000Z",
+      url: "https://swapi.dev/api/people/1/"
+    },
+    {
+      name: "C-3PO",
+      height: "167",
+      mass: "75",
+      hair_color: "n/a",
+      skin_color: "gold",
+      eye_color: "yellow",
+      birth_year: "112BBY",
+      gender: "n/a",
+      homeworld: "https://swapi.dev/api/planets/1/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/4/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: ["https://swapi.dev/api/species/2/"],
+      vehicles: [],
+      starships: [],
+      created: "2014-12-10T15:10:51.357000Z",
+      edited: "2014-12-20T21:17:50.309000Z",
+      url: "https://swapi.dev/api/people/2/"
+    },
+    {
+      name: "R2-D2",
+      height: "96",
+      mass: "32",
+      hair_color: "n/a",
+      skin_color: "white, blue",
+      eye_color: "red",
+      birth_year: "33BBY",
+      gender: "n/a",
+      homeworld: "https://swapi.dev/api/planets/8/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/4/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: ["https://swapi.dev/api/species/2/"],
+      vehicles: [],
+      starships: [],
+      created: "2014-12-10T15:11:50.376000Z",
+      edited: "2014-12-20T21:17:50.311000Z",
+      url: "https://swapi.dev/api/people/3/"
+    },
+    {
+      name: "Darth Vader",
+      height: "202",
+      mass: "136",
+      hair_color: "none",
+      skin_color: "white",
+      eye_color: "yellow",
+      birth_year: "41.9BBY",
+      gender: "male",
+      homeworld: "https://swapi.dev/api/planets/1/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: [],
+      vehicles: [],
+      starships: ["https://swapi.dev/api/starships/13/"],
+      created: "2014-12-10T15:18:20.704000Z",
+      edited: "2014-12-20T21:17:50.313000Z",
+      url: "https://swapi.dev/api/people/4/"
+    },
+    {
+      name: "Leia Organa",
+      height: "150",
+      mass: "49",
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "19BBY",
+      gender: "female",
+      homeworld: "https://swapi.dev/api/planets/2/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: [],
+      vehicles: ["https://swapi.dev/api/vehicles/30/"],
+      starships: [],
+      created: "2014-12-10T15:20:09.791000Z",
+      edited: "2014-12-20T21:17:50.315000Z",
+      url: "https://swapi.dev/api/people/5/"
+    },
+    {
+      name: "Owen Lars",
+      height: "178",
+      mass: "120",
+      hair_color: "brown, grey",
+      skin_color: "light",
+      eye_color: "blue",
+      birth_year: "52BBY",
+      gender: "male",
+      homeworld: "https://swapi.dev/api/planets/1/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: [],
+      vehicles: [],
+      starships: [],
+      created: "2014-12-10T15:52:14.024000Z",
+      edited: "2014-12-20T21:17:50.317000Z",
+      url: "https://swapi.dev/api/people/6/"
+    },
+    {
+      name: "Beru Whitesun lars",
+      height: "165",
+      mass: "75",
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "blue",
+      birth_year: "47BBY",
+      gender: "female",
+      homeworld: "https://swapi.dev/api/planets/1/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: [],
+      vehicles: [],
+      starships: [],
+      created: "2014-12-10T15:53:41.121000Z",
+      edited: "2014-12-20T21:17:50.319000Z",
+      url: "https://swapi.dev/api/people/7/"
+    },
+    {
+      name: "R5-D4",
+      height: "97",
+      mass: "32",
+      hair_color: "n/a",
+      skin_color: "white, red",
+      eye_color: "red",
+      birth_year: "unknown",
+      gender: "n/a",
+      homeworld: "https://swapi.dev/api/planets/1/",
+      films: ["https://swapi.dev/api/films/1/"],
+      species: ["https://swapi.dev/api/species/2/"],
+      vehicles: [],
+      starships: [],
+      created: "2014-12-10T15:57:50.959000Z",
+      edited: "2014-12-20T21:17:50.321000Z",
+      url: "https://swapi.dev/api/people/8/"
+    },
+    {
+      name: "Biggs Darklighter",
+      height: "183",
+      mass: "84",
+      hair_color: "black",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "24BBY",
+      gender: "male",
+      homeworld: "https://swapi.dev/api/planets/1/",
+      films: ["https://swapi.dev/api/films/1/"],
+      species: [],
+      vehicles: [],
+      starships: ["https://swapi.dev/api/starships/12/"],
+      created: "2014-12-10T15:59:50.509000Z",
+      edited: "2014-12-20T21:17:50.323000Z",
+      url: "https://swapi.dev/api/people/9/"
+    },
+    {
+      name: "Obi-Wan Kenobi",
+      height: "182",
+      mass: "77",
+      hair_color: "auburn, white",
+      skin_color: "fair",
+      eye_color: "blue-gray",
+      birth_year: "57BBY",
+      gender: "male",
+      homeworld: "https://swapi.dev/api/planets/20/",
+      films: [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/4/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+      ],
+      species: [],
+      vehicles: ["https://swapi.dev/api/vehicles/38/"],
+      starships: [
+        "https://swapi.dev/api/starships/48/",
+        "https://swapi.dev/api/starships/59/",
+        "https://swapi.dev/api/starships/64/",
+        "https://swapi.dev/api/starships/65/",
+        "https://swapi.dev/api/starships/74/"
+      ],
+      created: "2014-12-10T16:16:29.192000Z",
+      edited: "2014-12-20T21:17:50.325000Z",
+      url: "https://swapi.dev/api/people/10/"
+    }
+  ];
+  var heroes_default = {
+    count,
+    next,
+    previous,
+    results
+  };
+
   // index.js
   window.Alpine = module_default;
+  window.getHeroes = function() {
+    return heroes_default.results;
+  };
   module_default.start();
 })();
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import Alpine from "alpinejs";
+import Alpine from 'alpinejs';
+import fetchedData from './heroes.json';
 
 window.Alpine = Alpine;
+
+window.getHeroes = function () {
+  return fetchedData.results;
+};
 
 Alpine.start();
